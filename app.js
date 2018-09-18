912d03c069507cab1c2b9f969545e9fa4c52ca6c
vaffonso <vaffonso@br.ibm.com>
2018-09-18 11:35:26 -0300

    Include POST route

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
+const port = 3000;
+
+app.post('/imagem', function(req, res){
+  console.log(`I just received a POST request on port ${port}!`);
+  res.send('Hello World!');
+});
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
 });
